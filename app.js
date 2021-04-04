a57b042d4de102f5e43efcde9ca8f8b6e494738b
AbhishekAdarshMishra <abhishekmshr063@gmail.com>
2021-04-04 08:01:02 +0530

    model created = comment post user vote and router created login and register

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
 const express = require('express');
 const app = express();
+const morgan =require('morgan');
+const bodyParser = require('body-parser');
 
-app.use((req,res,next) => {
-    res.status(200).json({
-        message:'IT works'
-    });
+
+ const register_Routes = require('./api/routes/register');
+ const login_Routes = require('./api/routes/login');
+
+
+
+const mongoose = require('mongoose');
+
+const mongid='mongodb://localhost/kiitprj';
+const mong='mongodb+srv://kiitminor:kit@open@kiitminor.gawin.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose
+  .connect(mongid, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true })
+  .then(() => {
+    console.log('Connected to database!'); 
+  })
+  .catch(error => {
+    console.log('Connection failed!');
+    console.log(error);
+  });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
 });
-module.exports=app;
+
+app.use(morgan('dev'));
+app.use('/uploads',express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-with,Content-Type,Accept,Authorization");
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
+app.use('/register',register_Routes);
+app.use('/login',login_Routes);
+
+
+app.use((req,res,next) =>{
+     const error = new Error('Not Found');
+     error.status=404;
+     next(error);
+ });
+ 
+ app.use((error,req,res,next)=>{
+    res.status(error.status||500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+ });
+
+ module.exports = app;
